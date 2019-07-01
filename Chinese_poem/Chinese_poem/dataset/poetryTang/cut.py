f979ec465c0e8e955c135ae557f8e2cdd1ee52d5
suzijing <44896920+suzijing@users.noreply.github.com>
2019-07-01 11:58:35 +0800

    Add files via upload

---
+++ Chinese_poem/Chinese_poem/dataset/poetryTang/cut.py
@@ -0,0 +1,12 @@
+import re
+poemfile=open('poetryTang.txt',encoding='utf-8').read()
+p1 = r"[\u4e00-\u9fa5]*[\u3002|\uff0c]"
+pattern1 = re.compile(p1)
+
+
+result=pattern1.findall(poemfile)
+
+f=open('cut.txt','w',encoding='utf-8')
+for x in result:
+    f.write(x)
+f.close()
